1d24640206cba28e804aac92003a41cc1ac987eb
Overlord_Zoltron <jmarsenault@email.neit.edu>
2017-01-19 18:36:51 -0500

    added code for generating random movie

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movies_list = ["The Big Lebowski", "Nightcrawler", "Fury", "Lawless", "Sicario", "Spaceballs"]
 
         # TODO: randomly choose one of the movies, and return it
+        rand_movie = random.randrange(len(movies_list))
 
-        return "The Big Lebowski"
+        return movies_list[rand_movie]
 
     def get(self):
         # choose a movie by invoking our new function
@@ -20,6 +23,10 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
+        content += "<br />"
+        content += "<br />"
+        content += "<h1>Tomorrow's Movie</h1>"
+        content += "<p>" + movie + "</p>"
 
         self.response.write(content)
 
